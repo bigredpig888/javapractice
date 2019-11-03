ce9d4ee54df415959d6e40d51e0262cf02d83765
bigredpif888 <tengjian007@126.com>
2019-11-03 16:47:25 +0800

    aa

--- src/chapt1/createmode/factory/StudentWork.java
+++ src/chapt1/createmode/factory/StudentWork.java
@@ -6,6 +6,9 @@ public class StudentWork implements Work {
     @Override
     public void deWork() {
         System.out.println("学生做作业");
+        public String aa;
+        aa= "aa";
+        System.out.println(aa);
 
     }
 }
